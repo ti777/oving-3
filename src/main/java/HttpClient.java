6417e0784e8bf607369274ba9134c7d4364765be
ti777 <tifany090295@hotmail.com>
2021-09-19 21:31:39 +0200

    add timout

--- src/main/java/HttpClient.java
+++ src/main/java/HttpClient.java
@@ -5,10 +5,30 @@ import java.util.HashMap;
 
 public class HttpClient {
 
-    private final int statusCode;
-    
+    //private final int statusCode;
+
     //constructor
-    public HttpClient(String host, int port, String requestTarget) {
+    public HttpClient(String host, int port, String requestTarget) throws IOException {
+        Socket socket = new Socket(host, port);
+
+        String request = "GET " + requestTarget + " HTTP/1.1\r\n" +
+                "Host: " + host + "\r\n" +
+                "Connection: keep-alive\r\n" +
+                "Cache-Control: max-age=0\r\n" +
+                "Upgrade-Insecure-Requests: 1\r\n" +
+                "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36\r\n" +
+                "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\r\n" +
+                "Accept-Language: nb-NO,nb;q=0.9,no;q=0.8,nn;q=0.7,en-US;q=0.6,en;q=0.5\r\n";
+
+        socket.getOutputStream().write(request.getBytes());
+
+        //istedenfor å skrive ut (sout), vil vi ta vare på teksten i en stringbuilder
+        StringBuilder result = new StringBuilder();
+        int c;
+        while ((c = socket.getInputStream().read()) != -1) {
+            result.append((char) c);
+        }
+        System.out.println(result);
     }
 
     public static void main(String[] args) throws IOException {
@@ -30,8 +50,7 @@ public class HttpClient {
         }
     }
 
-
     public int getStatusCode() {
-        return statusCode;
+        return 0;
     }
 }
--- src/test/java/HttpClientTest.java
+++ src/test/java/HttpClientTest.java
@@ -6,13 +6,13 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class HttpClientTest {
     @Test
-    void shouldGetSuccessfulResponseCode() {
+    void shouldGetSuccessfulResponseCode() throws IOException {
         HttpClient client = new HttpClient("httpbin.org", 80, "/html");
         assertEquals(200, client.getStatusCode());
     }
 
     @Test
-    void shouldGetFailureResponseCode() {
+    void shouldGetFailureResponseCode() throws IOException {
         HttpClient client = new HttpClient("httpbin.org", 80, "/status/403");
         assertEquals(401, client.getStatusCode());
     }
